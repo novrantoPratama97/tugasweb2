9a71cb71920f1b8e7f02419c44bf54219267ec0e
novrantoPratama97 <81424184+novrantoPratama97@users.noreply.github.com>
2021-05-25 18:15:46 +0700

    Create tampilan.php

---
+++ tampilan.php
@@ -0,0 +1,41 @@
+<html>
+
+<head>
+<title>Contact</title>
+</head>
+<body>
+<h1>Lutfi Septian (171011401149)</h1>
+<a href="index.php"> Kembali ke Buku Tamu</a>
+<br>
+<h2>BUKU TAMU</h2>
+<a href="tampil.php"> Lihat Buku Tamu</a>
+<hr size=1>
+
+<?php
+include ("connect.php");
+// sql menampilkan record
+$sql = "SELECT id, name, email, address, city, msg FROM tamu";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data setiap baris
+    while($row = $result->fetch_assoc()) {
+
+  echo "<br>".
+  "id: ". $row["id"]. "<br>".
+  "Name: ". $row["name"]. "<br>".
+  "Email: ". $row["email"]. "<br>".
+  "Alamat: ". $row["address"]. "<br>".
+  "Kota: ". $row["city"]. "<br>".
+  "Pesan: ". $row["msg"]. "<br>";
+    }				
+	
+} else {
+    echo "0 results";
+}
+$conn->close()
+?>
+
+
+</body>
+</html>
